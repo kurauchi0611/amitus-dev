3079a6ca53f09853c265721adaa1d3cb94b6557b
kurauchi0611 <5kuu0611@gmail.com>
2020-02-25 18:30:44 +0900

    うんち！

---
+++ src/app/components/lecture/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import App from "./containers/App";
+import { Provider } from "react-redux";
+import configureStore from "./store";
+
+import {
+  // Box,
+  //   Button,
+  //   Grid,
+  //   Paper,
+  // Typography,
+  Container,
+  // CssBaseline
+  // Divider
+} from "@material-ui/core";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    margin: {
+      marginTop: theme.spacing(10),
+      background: "#fff",
+      padding: "0",
+      paddingBottom: theme.spacing(10)
+    },
+  })
+)
+
+export default () => {
+  const classes=useStyles();
+  const store = configureStore();
+  console.log(store);
+  
+  return (
+    <Container className={classes.margin}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Container>
+  );
+};
--- src/app/pages/lecture/index.tsx
+++ src/app/pages/lecture/index.tsx
@@ -1,45 +1,16 @@
+import dynamic from 'next/dynamic'
 
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import React from "react";
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    margin: { marginTop: theme.spacing(10) },
-    title: { background: "#fff", marginBottom: theme.spacing(1) },
-    error: {
-      background: theme.palette.buttonCancel.main
-    }
-  })
-);
+const DynamicComponentWithNoSSR = dynamic(
+  () => import('../../components/lecture/index'),
+  { ssr: false }
+)
 
-const Index = ({ props }) => {
-  const classes = useStyles();
-  const sampleMoji =
-    "# 助けて(´;ω;｀)\n```js\nconst arrowDisplayNone = () => {\n  document.querySelectorAll('.arrow').forEach(item => {\n    item.style.display = 'none';\n  }\n)}\n```";
-  const [state, setState] = React.useState<{
-    title: string;
-    tags: any;
-    text: any;
-    index: number;
-    userData: any;
-  }>({
-    title: "",
-    tags: [],
-    text: sampleMoji,
-    index: 0,
-    userData: props
-  });
-  React.useEffect(() => {
-    setState({ ...state, userData: props });
-    const Peer=require('skyway-js');
-    const peer = new Peer({key: 'e9d4c794-a70f-4334-a073-db4f40a7563a'});
-    console.log(peer);
-    
-  }, [props]);
+function Lecture() {
   return (
-    <React.Fragment>
-      <div className={classes.margin}></div>
-    </React.Fragment>
-  );
-};
+    <div>
+      <DynamicComponentWithNoSSR />
+    </div>
+  )
+}
 
-export default Index;
+export default Lecture
