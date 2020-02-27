27a5dcd788aee09b1818d88488b9ddfdc2d034a7
kurauchi0611 <5kuu0611@gmail.com>
2020-02-27 18:09:39 +0900

    なんとかなれ

--- src/app/components/lecture/index.tsx
+++ src/app/components/lecture/index.tsx
@@ -1,34 +1,33 @@
+import React from "react";
+import {App} from "./containers/App";
 import {
   // Box,
   //   Button,
   //   Grid,
   //   Paper,
   // Typography,
-  Container
+  Container,
   // CssBaseline
   // Divider
 } from "@material-ui/core";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import React from "react";
-import { App } from "./containers/App";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     margin: {
-      background: "#fff",
       marginTop: theme.spacing(10),
+      background: "#fff",
       padding: "0",
       paddingBottom: theme.spacing(10)
-    }
+    },
   })
-);
+)
 
-export default ({ props }) => {
-  const classes = useStyles();
+export default ({props}) => {
+  const classes=useStyles();
   return (
     <Container className={classes.margin}>
-      <App props={props} />
+        <App props={props} />
     </Container>
   );
 };
-
