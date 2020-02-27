c740d1ed40fbc3b2efcbb6e53633b439624ca32f
Enyou <park.sooneul@gmail.com>
2020-02-27 16:46:16 +0900

    トップページちょっと変更した

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -7,7 +7,7 @@ import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
-//import Grid from '@material-ui/core/Grid';
+import Grid from '@material-ui/core/Grid';
 import Pagination from '@material-ui/lab/Pagination';
 //import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
@@ -17,28 +17,23 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     wrap: {
       display: "flex",
-      flexFlow: "column",
-      justifyContent: "center",
-      alignItems: "center",
+      flexFlow: "row",
+      justifyContent: "space-around",
+      // alignItems: "center",
       textAlign: "center",
-      marginTop: theme.spacing(10)
-    },
-
-    root: {
-      display: "flex",
+      marginTop: theme.spacing(10),
+      background:theme.palette.background.paper,
     },
 
     left: {
       marginLeft: theme.spacing(2),
       backgroundColor: theme.palette.background.paper,
-      width: 700,
       paddingTop: theme.spacing(2)
     },
 
     right: {
       marginRight: theme.spacing(2),
       backgroundColor: theme.palette.background.paper,
-      width: 500,
       paddingTop: theme.spacing(2)
     },
 
@@ -108,118 +103,114 @@ export default function FullWidthTabs() {
   };
 
   return (
-    <div className={classes.wrap}>
-      <div className={classes.root}>
-        <Container className={classes.left}>
-          <AppBar position="static" color="default">
-            <Tabs
-              value={value}
-              onChange={handleChange}
-              indicatorColor="primary"
-              textColor="primary"
-              variant="fullWidth"
-              aria-label="tabs"
-            >
-              <Tab label="人気の質問" {...a11yProps(0)} />
-              <Tab label="マイスキルの質問" {...a11yProps(1)} />
-              <Tab label="人気のチケット" {...a11yProps(2)} />
-            </Tabs>
-          </AppBar>
-          <SwipeableViews
-            axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-            index={value}
-            onChangeIndex={handleChangeIndex}
+    <Container className={classes.wrap}>
+      <Grid className={classes.left} xs={6}>
+        <AppBar position="static" color="default">
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            variant="fullWidth"
+            aria-label="tabs"
           >
-            <TabPanel value={value} index={0} dir={theme.direction}>
+            <Tab label="人気の質問" {...a11yProps(0)} />
+            <Tab label="マイスキルの質問" {...a11yProps(1)} />
+            <Tab label="人気のチケット" {...a11yProps(2)} />
+          </Tabs>
+        </AppBar>
 
-              <Paper className={classes.paper}>
-                Content<br></br>Content<br></br>Content
-              </Paper>
-              <Paper className={classes.paper}>
-                Content<br></br>Content<br></br>Content
-              </Paper>
-              <Paper className={classes.paper}>
-                Content<br></br>Content<br></br>Content
-              </Paper>
-              <Paper className={classes.paper}>
-                Content<br></br>Content<br></br>Content
-              </Paper>
-              <Paper className={classes.paper}>
-                Content<br></br>Content<br></br>Content
-              </Paper>
-              
-              <Pagination className={classes.center} count={10} color="primary" />
-            </TabPanel>
+        <SwipeableViews
+          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+          index={value}
+          onChangeIndex={handleChangeIndex}
+        >
+          <TabPanel value={value} index={0} dir={theme.direction}>
+            <Paper className={classes.paper}>
+              Content<br></br>Content<br></br>Content
+            </Paper>
+            <Paper className={classes.paper}>
+              Content<br></br>Content<br></br>Content
+            </Paper>
+            <Paper className={classes.paper}>
+              Content<br></br>Content<br></br>Content
+            </Paper>
+            <Paper className={classes.paper}>
+              Content<br></br>Content<br></br>Content
+            </Paper>
+            <Paper className={classes.paper}>
+              Content<br></br>Content<br></br>Content
+            </Paper>
+            
+            <Pagination className={classes.center} count={10} color="primary" />
+          </TabPanel>
 
-            <TabPanel value={value} index={1} dir={theme.direction}>
+          <TabPanel value={value} index={1} dir={theme.direction}>
+            <Paper className={classes.paper}>
+              Content<br></br>Content<br></br>Content
+            </Paper>
+            <Paper className={classes.paper}>
+              Content<br></br>Content<br></br>Content
+            </Paper>
+            <Paper className={classes.paper}>
+              Content<br></br>Content<br></br>Content
+            </Paper>
+            <Paper className={classes.paper}>
+              Content<br></br>Content<br></br>Content
+            </Paper>
+            <Paper className={classes.paper}>
+              Content<br></br>Content<br></br>Content
+            </Paper>
+            
+            <Pagination className={classes.center} count={10} color="primary" />
+          </TabPanel>
 
-              <Paper className={classes.paper}>
-                Content<br></br>Content<br></br>Content
-              </Paper>
-              <Paper className={classes.paper}>
-                Content<br></br>Content<br></br>Content
-              </Paper>
-              <Paper className={classes.paper}>
-                Content<br></br>Content<br></br>Content
-              </Paper>
-              <Paper className={classes.paper}>
-                Content<br></br>Content<br></br>Content
-              </Paper>
-              <Paper className={classes.paper}>
-                Content<br></br>Content<br></br>Content
-              </Paper>
-              
-              <Pagination className={classes.center} count={10} color="primary" />
-            </TabPanel>
+          <TabPanel value={value} index={2} dir={theme.direction}>
+            <Paper className={classes.paper}>
+              Content<br></br>Content<br></br>Content
+            </Paper>
+            <Paper className={classes.paper}>
+              Content<br></br>Content<br></br>Content
+            </Paper>
+            <Paper className={classes.paper}>
+              Content<br></br>Content<br></br>Content
+            </Paper>
+            <Paper className={classes.paper}>
+              Content<br></br>Content<br></br>Content
+            </Paper>
+            <Paper className={classes.paper}>
+              Content<br></br>Content<br></br>Content
+            </Paper>
+            
+            <Pagination className={classes.center} count={10} color="primary" />
+          </TabPanel>
+        </SwipeableViews>
+      </Grid>
+      
+      <Grid className={classes.right} xs={5}>
+        <Paper className={classes.paper}>
+          <Box textAlign="left" fontSize="h6.fontSize">本日のスケジュール</Box>
+          <Box m={5}>更新中・・・</Box>
+          <Box textAlign="right"><Button variant="outlined">一覧</Button></Box>
+        </Paper>
 
-            <TabPanel value={value} index={2} dir={theme.direction}>
-              
-              <Paper className={classes.paper}>
-                Content<br></br>Content<br></br>Content
-              </Paper>
-              <Paper className={classes.paper}>
-                Content<br></br>Content<br></br>Content
-              </Paper>
-              <Paper className={classes.paper}>
-                Content<br></br>Content<br></br>Content
-              </Paper>
-              <Paper className={classes.paper}>
-                Content<br></br>Content<br></br>Content
-              </Paper>
-              <Paper className={classes.paper}>
-                Content<br></br>Content<br></br>Content
-              </Paper>
-              
-              <Pagination className={classes.center} count={10} color="primary" />
-            </TabPanel>
-          </SwipeableViews>
-        </Container>
-        
-        <Container className={classes.right}>
-          <Paper className={classes.paper}>
-            <Box textAlign="left" fontSize="h6.fontSize" m={2}>本日のスケジュール</Box>
-            <Box m={5}>更新中・・・</Box>
-            <Box textAlign="right"><Button variant="outlined">一覧</Button></Box>
-          </Paper>
-
-          <Box className={classes.margin} fontSize="h5.fontSize" m={1} textAlign="left">新着通知</Box>
-          <Paper className={classes.paper}>
-            <Box textAlign="left" fontSize="h6.fontSize">通知1タイトル</Box>
-            <Box m={3}>通知1の内容だよ・・</Box>
-            <Box textAlign="right">hh:mm:ss yyyy-mm-dd</Box>
-          </Paper>
-          <Paper className={classes.paper}>
-            <Box textAlign="left" fontSize="h6.fontSize">通知2タイトル</Box>
-            <Box m={3}>通知2の内容だよ・・</Box>
-            <Box textAlign="right">hh:mm:ss yyyy-mm-dd</Box>
-          </Paper>
-          <Paper className={classes.paper}>
-            <Box textAlign="left" fontSize="h6.fontSize">通知3タイトル</Box>
-            <Box m={3}>通知3の内容だよ・・</Box>
-            <Box textAlign="right">hh:mm:ss yyyy-mm-dd</Box>
-          </Paper>
-        </Container>
-      </div>
-    </div>
+        <Box className={classes.margin} fontSize="h5.fontSize" m={1} textAlign="left">新着通知</Box>
+        <Paper className={classes.paper}>
+          <Box textAlign="left" fontSize="h6.fontSize">通知1タイトル</Box>
+          <Box m={3}>通知1の内容だよ・・</Box>
+          <Box textAlign="right">hh:mm:ss yyyy-mm-dd</Box>
+        </Paper>
+        <Paper className={classes.paper}>
+          <Box textAlign="left" fontSize="h6.fontSize">通知2タイトル</Box>
+          <Box m={3}>通知2の内容だよ・・</Box>
+          <Box textAlign="right">hh:mm:ss yyyy-mm-dd</Box>
+        </Paper>
+        <Paper className={classes.paper}>
+          <Box textAlign="left" fontSize="h6.fontSize">通知3タイトル</Box>
+          <Box m={3}>通知3の内容だよ・・</Box>
+          <Box textAlign="right">hh:mm:ss yyyy-mm-dd</Box>
+        </Paper>
+      </Grid>
+    </Container>
   );
 }
